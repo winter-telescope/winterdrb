5b76f704a07e269c65ef3b0a53c735f9beae21d0
viraj21197 <karambelkarvraj21197@gmail.com>
2023-10-05 16:33:55 -0700

    add limit

--- winterdrb/database/query.py
+++ winterdrb/database/query.py
@@ -8,12 +8,14 @@ from mirar.database.transactions import select_from_table
 from winterdrb.models import RealBogus
 
 
-def get_night_db_entries(night, include_classified: bool = False) -> pd.DataFrame:
+def get_night_db_entries(night, include_classified: bool = False,
+                         limit: int = None) -> pd.DataFrame:
     """
     Get all entries from the night database for a given night
 
     :param night: Night to get entries for
     :param include_classified: Boolean to include classified entries
+    :param limit: Limit the number of entries returned
     :return: Datframe with entries
     """
     constraint = DBQueryConstraints("night", night, "=")
@@ -21,4 +23,6 @@ def get_night_db_entries(night, include_classified: bool = False) -> pd.DataFram
     if not include_classified:
         constraint.add_constraint("humanclass", 1, "<")
 
-    return select_from_table(constraint, RealBogus.sql_model)
+    return select_from_table(constraint, RealBogus.sql_model,
+                             max_num_results=limit)
+
