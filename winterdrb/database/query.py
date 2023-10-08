fabec2ad210ab37c991772eba59201dec364a872
viraj21197 <karambelkarvraj21197@gmail.com>
2023-10-08 11:38:56 -0700

    show unknown

--- winterdrb/database/query.py
+++ winterdrb/database/query.py
@@ -9,19 +9,24 @@ from winterdrb.models import RealBogus
 
 
 def get_night_db_entries(night, include_classified: bool = False,
+                         include_unknown: bool = False,
                          max_num_results: int = None) -> pd.DataFrame:
     """
     Get all entries from the night database for a given night
 
     :param night: Night to get entries for
     :param include_classified: Boolean to include classified entries
+    :param include_unknown: Boolean to include unknown entries
     :param max_num_results: Limit the number of entries returned
     :return: Datframe with entries
     """
     constraint = DBQueryConstraints("night", night, "=")
 
+    if include_unknown:
+        constraint.add_constraint("humanclass", 0, "=")
+
     if not include_classified:
-        constraint.add_constraint("humanclass", 0, "<")
+        constraint.add_constraint("humanclass", -1, "=")
 
     return select_from_table(constraint, RealBogus.sql_model,
                              max_num_results=max_num_results)
